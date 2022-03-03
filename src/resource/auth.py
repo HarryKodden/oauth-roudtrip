d7cd26d8b14bd5979d42cf8cb297df5625c23915
Harry Kodden <harry@e-tunity.nl>
2022-03-03 10:19:44 +0100

    cache issuer keys

--- src/resource/auth.py
+++ src/resource/auth.py
@@ -6,39 +6,52 @@ import requests
 
 from jwcrypto import jwk
 
+cached_keys = {}
 
-def verify_access_token(access_token):
-  try:
-    ISSUER = os.environ['ISSUER']
+def lookup_keys(issuer):
 
-    r = requests.get(ISSUER+'/.well-known/openid-configuration')
+  if issuer not in cached_keys:
+    r = requests.get(issuer+'/.well-known/openid-configuration')
     if r.status_code != 200:
       raise Exception("Missing issuer configuration!")
 
     r = requests.get(json.loads(r.text)['jwks_uri'])
     if r.status_code != 200:
-          raise Exception("Missing jwks configuration!")
+      raise Exception("Missing jwks configuration!")
 
-    keys = r.json()['keys']
-    for k in keys:
-      if k['use'] == 'sig':
-        key = jwk.JWK(**k)
+    cached_keys[issuer] = r.json()['keys']
 
-        data = jwt.decode(access_token, key.export_to_pem(),
-                               issuer = ISSUER,
-                               algorithms = 'RS256')
+  return cached_keys.get(issuer, [])
+
+
+def verify_access_token(access_token):
+  try:
+    ISSUER = os.environ['ISSUER']
+
+    for key in lookup_keys(ISSUER):
+      if key['use'] == 'sig':
+
+        data = jwt.decode(
+          access_token,
+          jwk.JWK(**key).export_to_pem(),
+          issuer = ISSUER,
+          algorithms = 'RS256'
+        )
+        
         logging.debug(data)
 
         return True
     
     logging.error("No signature key found !")
 
-  except (jwt.exceptions.InvalidTokenError,
-          jwt.exceptions.InvalidSignatureError,
-          jwt.exceptions.InvalidIssuerError,
-          jwt.exceptions.ExpiredSignatureError):
-    logging.error("No valid JWT access token !")
-
+  except jwt.exceptions.InvalidTokenError:
+    logging.error("Invalid access token !")
+  except jwt.exceptions.InvalidSignatureError:
+    logging.error("Invalid signature in access token !")
+  except jwt.exceptions.InvalidIssuerError:
+    logging.error("Invalid Issuer in access token !")
+  except jwt.exceptions.ExpiredSignatureError:
+    logging.error("Access Token is expired !")
   except Exception as e:
     logging.error("Error during access token verification: {}".format(str(e)))
 
